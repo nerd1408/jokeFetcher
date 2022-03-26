da28d48f96ee214deba65fb8a090a19cd6341cbb
maestrowscki10 <abdulhansen.ah@gmail.com>
2022-03-26 18:07:34 +0300

    this is fetch API for external links

---
+++ index.js
@@ -0,0 +1,37 @@
+document.querySelector('.get-jokes').addEventListener('click',getJokes)
+
+function getJokes(e) {
+    const number = document.querySelector('input[type="number"]').value  
+    const xhr = new XMLHttpRequest
+  xhr.open('GET',`http://api.icndb.com/jokes/random/${number}
+  `,true)
+
+xhr.onload = function () { 
+    if (this.status === 200)
+    { const response =JSON.parse(this.responseText)
+        console.log(response);
+      let output = ''
+      
+      if (response.type='success')
+      {
+
+       response.value.forEach(joke => {
+           
+      output += `<ul>id:${joke.id}<li>
+      <li>joke:${joke.joke}</li>`
+      
+      
+});
+ } else {
+          output += '<li>something went wrong </li>'
+          }
+          document.querySelector('.jokes').innerHTML = output
+    }
+}
+ 
+  xhr.send()
+
+    e.preventDefault()
+}
+
+
